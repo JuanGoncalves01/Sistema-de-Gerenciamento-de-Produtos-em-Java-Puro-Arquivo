00bd03f9c91ce704c7750adcbc9a66a93750b78b
JUAN GONÇALVES MACHADO <juan_g_machado@estudante.sesisenai.org.br>
2024-11-11 19:25:13 -0300

    Implementação do GerenciadorProdutos

--- untitled/src/GerenciadorProdutos.java
+++ untitled/src/GerenciadorProdutos.java
@@ -1,3 +1,51 @@
+
+
+import com.loja.modelo.Produto;
+import java.util.ArrayList;
+import java.util.List;
+
 public class GerenciadorProdutos {
+    private List<Produto> produtos = new ArrayList<>();
+    private int proximoId = 1;
+
+    public void criar(Produto produto) {
+        produto.setId(proximoId++);
+        produtos.add(produto);
+    }
+
+    public Produto buscarPorId(int id) {
+       for(Produto produto:produtos){
+           if (produto.getId()==id){
+               return produto;
+           }
+       }
+       return null;
+    }
+
+    public List<Produto> listarTodos() {
+        return new ArrayList<>(produtos);
+    }
+
+    public boolean atualizar(Produto produto) {
+        Produto existente = buscarPorId(produto.getId());
+        if (existente != null) {
+            existente.setNome(produto.getNome());
+            existente.setPreco(produto.getPreco());
+            existente.setQuantidadeEstoque(produto.getQuantidadeEstoque());
+            existente.setCategoria(produto.getCategoria());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deletar(int id) {
+        for (Produto produto : produtos) {
+            if (produto.getId() == id) {
+                produtos.remove(produto);
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
--- untitled/src/com/loja/modelo/Produto.java
+++ untitled/src/com/loja/modelo/Produto.java
@@ -1,4 +1,4 @@
-
+package com.loja.modelo;
 
 import java.util.Objects;
 
